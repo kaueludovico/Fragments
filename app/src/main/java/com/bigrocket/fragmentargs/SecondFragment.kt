da1ab7b544bfcb8cd125d88aa1e5500fa570a3d7
kaueludovico <kaueludovico@outlook.com>
2022-12-19 14:02:18 -0300

    Passando dados de Activity para fragment

---
+++ app/src/main/java/com/bigrocket/fragmentargs/SecondFragment.kt
@@ -0,0 +1,15 @@
+package com.bigrocket.fragmentargs
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+class SecondFragment : Fragment(R.layout.fragment_second) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
+
